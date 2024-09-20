3fedec5ac737fd16cd1ba81ee721f4dab9e670f3
MikeScriptMaster <168045109+dev-mmaisonneuve@users.noreply.github.com>
2024-09-19 21:04:43 -0400

    Just getting comfortble

---
+++ Python_Lessons/m4-intro-dict.py
@@ -0,0 +1,24 @@
+emails = {
+    'Anne Stahl': 'astahl@gmail.com',
+    'Peter Small': 'peter@yandex.com',
+    'Mark Steel': 'mark@steel.com'
+}
+print(emails['Mark Steel'])
+
+
+spanish_animals = {
+    'dog': 'el perro',
+    'cat': 'el gato',
+    'horse': 'el caballo',
+    'bird': 'el pajaro'
+}
+print(spanish_animals['bird'])
+
+
+tennis_ranking = {
+    1: 'Ashleigh Barty',
+    2: 'Naomi Osaka',
+    3: 'Simona Halep'
+
+}
+print(tennis_ranking[1])
