13fff7815c81d0e484b26f0121b6e4833823db45
MikeScriptMaster <168045109+dev-mmaisonneuve@users.noreply.github.com>
2024-09-20 22:59:59 -0400

    new  concepts

---
+++ Python_Lessons/m5-exception-hierarchy.py
@@ -0,0 +1,7 @@
+age = (input('What is your age? '))
+print('in 10 years, you will be', age + 10)
+# This will throw an error
+
+
+age = int(input('What is your age? '))
+print('in 10 years, you will be', age + 10)
