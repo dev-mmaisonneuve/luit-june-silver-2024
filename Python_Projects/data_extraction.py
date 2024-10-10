1725e67211ea86c144a2132f2b9d192ade66d648
Mike Maisonneuve <168045109+dev-mmaisonneuve@users.noreply.github.com>
2024-10-09 21:27:29 -0400

    updating

--- Python_Projects/data_extraction.py
+++ Python_Projects/data_extraction.py
@@ -1,35 +1,24 @@
 import os
 
-# Function to gather information about files in the current directory
-def get_files_info():
-    # Create an empty list to store file information
-    files_info = []
+# Get the current working directory
+current_directory = os.getcwd()
 
-    # Get a list of all files and folders in the current directory
-    for file_name in os.listdir():
-        # Check if the item is a file (ignore folders)
-        if os.path.isfile(file_name):
-            # Get the file size (in bytes)
-            file_size = os.path.getsize(file_name)
+# Initialize an empty list to hold the file information
+files_info = []
 
-            # Get the file type (extension), e.g., '.txt', '.py'
-            file_type = os.path.splitext(file_name)[1]
+# Loop through all items in the current directory
+for item in os.listdir(current_directory):
+    item_path = os.path.join(current_directory, item)
 
-            # Create a dictionary to store details about the file
-            file_info = {
-                'file_name': file_name,
-                'file_size': file_size,
-                'file_type': file_type
-            }
+    # Check if the item is a file (not a directory)
+    if os.path.isfile(item_path):
+        # Get file information: name and size
+        file_info = {
+            'name': item,
+            'size': os.path.getsize(item_path)
+        }
+        # Add the file information to the list
+        files_info.append(file_info)
 
-            # Add the file information to the list
-            files_info.append(file_info)
-
-    # Return the list of file information
-    return files_info
-
-# Call the function and store the result in a variable
-files_info_list = get_files_info()
-
-# Print the list of file information
-print(files_info_list)
+# Print the list of dictionaries containing file information
+print(files_info)
